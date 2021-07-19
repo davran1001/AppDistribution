bee151239caca3c94faad971704c3ab549bd5989
davran1001 <khasanovdavran10@gmail.com>
2021-07-20 01:27:12 +0600

    update comment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //empty comments
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
